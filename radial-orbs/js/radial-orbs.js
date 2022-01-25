e4fef0d4d86edaf2310accf80e7c0c5059828f87
Corex <sabenyogit@gmail.com>
2022-01-25 13:50:33 -0500

    Add animation to radial-orbs

--- radial-orbs/js/radial-orbs.js
+++ radial-orbs/js/radial-orbs.js
@@ -7,28 +7,20 @@ window.onload = function() {
   paper.setup(canvas);
 
   // Background
-  let background = new Path.Rectangle([0,0], view.size);
+  let background = new Path.Rectangle([0, 0], view.size);
   background.fillColor = 'black';
 
   // Parameters
   let nLevels = 5;
-  let nArms = 8;
+  let nArms = 14;
   let armAngle = 2 * Math.PI / nArms;
   let centerRadius = 100;
+  let centralOrb = {};
+  let arms = [];
+  let orbitalGroup = new Group();
 
   // Central orb
   let base = 0;
-  let centralOrb = new Path.Circle(view.center, centerRadius)
-  centralOrb.fillColor = {
-    gradient: {
-      stops: [[{hue: base + 10, saturation: 1, brightness: 1}, 0.2],
-              [{hue: base + 90, saturation: 1, brightness: 1}, 0.4],
-              [{hue: base + 180, saturation: 1, brightness: 1}, 1]],
-      radial: true
-    },
-    origin: centralOrb.position,
-    destination: centralOrb.bounds.rightCenter
-  }
 
   class Orb {
     constructor(radius, distance, angle, color) {
@@ -36,40 +28,79 @@ window.onload = function() {
       this.distance = distance;
       this.angle = angle;
       this.color = color;
-      this.center = view.center.add([distance * Math.cos(angle),distance * Math.sin(angle)]);
+      this.center = view.center.add([distance * Math.cos(angle), distance * Math.sin(angle)]);
       this.orb = new Path.Circle(this.center, this.radius);
+    }
 
+    setGradient() {
       this.orb.fillColor = {
         gradient: {
-          stops: [[{hue: color, saturation: 1, brightness: 1}, 0.8],
-                 [{hue: color, saturation: 1, brightness: 0.3}, 1]],
+          stops: [[{hue: this.color, saturation: 1, brightness: 1}, 0.8],
+            [{hue: this.color, saturation: 1, brightness: 0.3}, 1]],
           radial: true
         },
         origin: this.orb.position,
         destination: this.orb.bounds.rightCenter
+      };
+    }
+  }
+
+  function generateBaseOrbs() {
+    for (let angle = 0; angle < 2 * Math.PI; angle += armAngle) {
+      let arm = []
+      let orb = new Orb(0.2 * centerRadius, centerRadius, angle, 200);
+      orb.setGradient();
+      arm.push(orb);
+      arms.push(arm);
+    }
+  }
+
+  function generateOrbArms() {
+    for (let arm of arms) {
+      for (let level = 1; level <= nLevels; level++) {
+        let nextRadius = arm[level - 1].radius * 1.3;
+        let nextDistance = arm[level - 1].distance + nextRadius;
+        let nextColor = arm[level - 1].color - 10;
+        let orb = new Orb(nextRadius, nextDistance, arm[level - 1].angle, nextColor);
+        orb.setGradient();
+        arm.push(orb);
       }
     }
   }
 
-  // Generate first level/ring of orbs
-  let arms = [];
-  for(let angle = 0; angle < 2 * Math.PI; angle += armAngle) {
-    let arm = []
-    let orb = new Orb(0.2 * centerRadius, centerRadius, angle, 200);
-    arm.push(orb);
-    arms.push(arm);
-  }
-
-  // Generate remaining orbs for each level/ring, for each arm
-  for(let arm of arms) {
-    for(let level = 1; level <= nLevels; level++) {
-      let nextRadius = arm[level - 1].radius * 1.3;
-      let nextDistance = arm[level - 1].distance + nextRadius;
-      let nextColor = arm[level - 1].color - 10;
-      let orb = new Orb(nextRadius, nextDistance, arm[level - 1].angle, nextColor);
-      arm.push(orb);
+  function generateCentralOrb() {
+    centralOrb = new Path.Circle(view.center, centerRadius)
+    centralOrb.fillColor = {
+      gradient: {
+        stops: [[{hue: base + 10, saturation: 1, brightness: 1}, 0.2],
+          [{hue: base + 90, saturation: 1, brightness: 1}, 0.4],
+          [{hue: base + 220, saturation: 1, brightness: 0.7}, 1]],
+        radial: true
+      },
+      origin: centralOrb.position,
+      destination: centralOrb.bounds.rightCenter
     }
   }
 
-  centralOrb.bringToFront();
+  function modulateGradient() {
+    for (let arm of arms) {
+      for (let level = 0; level <= nLevels; level++) {
+        let newColor = arm[level].color;
+        arm[level].color = newColor + 1;
+        arm[level].setGradient();
+
+      }
+    }
+  }
+
+  generateBaseOrbs();
+  generateOrbArms();
+  generateCentralOrb();
+
+
+
+  view.onFrame = function(event) {
+    modulateGradient();
+  }
+
 }
