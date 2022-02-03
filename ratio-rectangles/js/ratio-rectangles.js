79545332c97250dfe7bea46a5ec872c4d8cb8394
Corex <sabenyogit@gmail.com>
2022-02-03 09:06:57 -0500

    Implement ratio-rectangles

--- ratio-rectangles/js/ratio-rectangles.js
+++ ratio-rectangles/js/ratio-rectangles.js
@@ -10,68 +10,61 @@ window.onload = function() {
   let background = new Path.Rectangle([0,0], view.size);
   background.fillColor = 'black';
 
-  // Determine radius/diameter based on horizontal number of circles
-  let nWide = 15;
-  let diameter = view.size.width / nWide;
-  let radius = diameter / 2;
-  let nHigh = view.size.height / diameter;
-  let start = new Point(radius, radius);
+  // Parameters
+  let nDivisions = 5;
+  let ratio = 0.5;
 
-  let seed = 0;
-  let outline = 0;
+  // Generate divisions
+  let xCenter = view.center.x;
+  let xFirst = 0;
+  let xLast = view.size.width;
+  let yCenter = view.center.y;
+  let yFirst = 0;
+  let yLast = view.size.height;
 
-  // Generate grid of circles
-  function generateGrid(base) {
-    for(let i = 0; i < nWide; i++) {
-      for (let j = 0; j < nHigh; j++) {
+  let xLeft = [];
+  let xRight = [];
+  let yTop = [];
+  let yBottom = [];
 
-        // Determine center
-        let center = [start.x + diameter * i, start.y + diameter * j]
-        let circle = new Path.Circle(center, radius);
+  xLeft.push(xFirst);
+  xRight.push(xLast);
+  yTop.push(yFirst);
+  yBottom.push(yLast)
 
-        // Add gradient, modulate color based on location
-        circle.fillColor = {
-          gradient: {
-            stops: [[{hue: base + 10 * i + 20 * j, saturation: 1, brightness: 1}, 0.1],
-                    [{hue: base + 90 + 10 * i + 20 * j, saturation: 1, brightness: 1}, 0.4],
-                    [{hue: base + 180 + 10 * i + 20 * j, saturation: 1, brightness: 1}, 1]],
-            radial: true
-          },
-          origin: circle.position,
-          destination: circle.bounds.rightCenter
-        }
+  let xHorizontal = [];
+  let yVertical = [];
 
-        // Outline circle on mouse hover
-        circle.onMouseEnter = function(event) {
-          this.strokeColor = 'white'
-          this.strokeWidth = 10;
-        }
-
-        circle.onMouseLeave = function(event) {
-          this.strokeWidth = 0;
-        }
-
-        // Change color of grid on mouse click
-        circle.onClick = function(event) {
-          //seed = this.fillColor.hue;
-          console.log(this.fillColor.gradient.stops[0].color.components[0]);
-          seed = this.fillColor.gradient.stops[0].color.components[0];
-          generateGrid(seed);
-        }
-      }
-    }
+  // Generate horizontal and vertical divisions
+  for (let i = 1; i <= nDivisions; i++) {
+    let prevXLeft = xLeft[i - 1];
+    let nextXLeft = prevXLeft + ratio * (xCenter - prevXLeft);
+    xLeft.push(nextXLeft);
+    let prevXRight = xRight[i - 1];
+    let nextXRight = prevXRight - ratio * (prevXRight - xCenter);
+    xRight.push(nextXRight);
+    let prevYTop = yTop[i - 1];
+    let nextYTop = prevYTop + ratio * (yCenter - prevYTop);
+    yTop.push(nextYTop);
+    let prevYBottom = yBottom[i - 1];
+    let nextYBottom = prevYBottom - ratio * (prevYBottom - yCenter);
+    yBottom.push(nextYBottom);
   }
 
-  generateGrid(seed);
+  // Reverse right-side, combine with left-side
+  xRight.reverse();
+  xHorizontal.push(...xLeft);
+  xHorizontal.push(...xRight);
+  yBottom.reverse();
+  yVertical.push(...yTop);
+  yVertical.push(...yBottom);
 
+  for (let element of xHorizontal) {
+    console.log("x ", element);
+  }
 
-
-
-
-
-
-
-
-
+  for (let element of yVertical) {
+    console.log("y ", element);
+  }
 
 }
